c67f53e566d0f11682f5ea09ca0a52e2cce0c777
hita1999 <41143969+hita1999@users.noreply.github.com>
2023-07-27 15:31:56 +0900

    fix: nx.draw() to nx.draw_networkx()

--- montecarlo_3_3.py
+++ montecarlo_3_3.py
@@ -120,8 +120,8 @@ edge_norm = plt.Normalize(min(edge_weights), max(edge_weights))
 
 # Draw the graph with edge colors and colorbar
 plt.figure(2)
-nx.draw(G, pos, node_color='k', edge_color=edge_weights, edge_cmap=plt.cm.cool,
-        with_labels=True, width=2, edge_vmin=min(edge_weights), edge_vmax=max(edge_weights))
+nx.draw_networkx(G, pos, node_color='k', edge_color=edge_weights, edge_cmap=plt.cm.cool,
+                 with_labels=True, width=2, edge_vmin=min(edge_weights), edge_vmax=max(edge_weights))
 c = plt.colorbar(cm.ScalarMappable(norm=edge_norm, cmap=plt.cm.cool))
 c.set_label("Edge Weight")
 plt.show()
