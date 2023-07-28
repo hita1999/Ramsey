70f7d9a8efdf801bc34e978628cc47469b8247fb
hita1999 <41143969+hita1999@users.noreply.github.com>
2023-07-28 12:52:44 +0900

    add: draw function

---
+++ adjcencyListToMatrix.py
@@ -0,0 +1,43 @@
+import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
+
+def read_adj_list(file_path):
+    adj_list = {}
+    with open(file_path, 'r') as file:
+        for i, line in enumerate(file):
+            neighbors = list(map(int, line.strip().split()))
+            adj_list[i] = neighbors
+    return adj_list
+
+def adj_list_to_adj_matrix(adj_list, num_nodes):
+    adj_matrix = np.zeros((num_nodes, num_nodes), dtype=int)
+    for node, neighbors in adj_list.items():
+        for neighbor in neighbors:
+            adj_matrix[node][neighbor] = 1
+    return adj_matrix
+
+file_path = 'adjcencyMatrix/R(3_11_44)_list.txt'
+adj_list = read_adj_list(file_path)
+num_nodes = len(adj_list)
+adj_matrix = adj_list_to_adj_matrix(adj_list, num_nodes)
+print(adj_matrix)
+
+# 行列の対角線以外の0を2に変更する
+for i in range(len(adj_matrix)):
+    for j in range(len(adj_matrix[i])):
+        if i != j and adj_matrix[i][j] == 0:
+            adj_matrix[i][j] = 2
+
+
+# グラフを作成
+G = nx.from_numpy_array(np.array(adj_matrix))
+
+# グラフの描画
+weights = nx.get_edge_attributes(G, 'weight')
+colors = ['red' if weights[e] == 1 else 'blue' for e in G.edges]
+
+plt.figure(figsize=(10, 10))
+pos = nx.circular_layout(G)
+nx.draw(G, pos, with_labels=True, node_color='lightblue', node_size=1000, edge_color=colors, width=2.0)
+plt.show()
---
+++ drawGraph.py
@@ -0,0 +1,33 @@
+import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
+
+def read_adjacency_matrix(file_path):
+    adjacency_matrix = []
+    with open(file_path, 'r') as file:
+        for line in file:
+            row = [int(x) for x in line.strip()]
+            adjacency_matrix.append(row)
+    return adjacency_matrix
+
+file_path = 'adjcencyMatrix/R(3_12_52).txt'
+adjacency_matrix = read_adjacency_matrix(file_path)
+
+# 行列の対角線以外の0を2に変更する
+for i in range(len(adjacency_matrix)):
+    for j in range(len(adjacency_matrix[i])):
+        if i != j and adjacency_matrix[i][j] == 0:
+            adjacency_matrix[i][j] = 2
+
+
+# グラフを作成
+G = nx.from_numpy_array(np.array(adjacency_matrix))
+
+# グラフの描画
+weights = nx.get_edge_attributes(G, 'weight')
+colors = ['red' if weights[e] == 1 else 'blue' for e in G.edges]
+
+plt.figure(figsize=(10, 10))
+pos = nx.circular_layout(G)
+nx.draw(G, pos, with_labels=True, node_color='lightblue', node_size=1000, edge_color=colors, width=2.0)
+plt.show()
