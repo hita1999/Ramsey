3df380af7b4aa92d024b5b8afca07436bfbe42f1
hita1999 <41143969+hita1999@users.noreply.github.com>
2023-12-18 19:38:43 +0900

    update

--- generate_random_binary_sequence.py
+++ generate_random_binary_sequence.py
@@ -5,11 +5,12 @@ def generate_random_binary_sequence(binary_sequence):
     random.shuffle(shuffled_sequence)
     return ''.join(shuffled_sequence)
 
-# 01の数列
-binary_sequence = "1010101010101010"
+if __name__ == "__main__":
+    # 01の数列
+    binary_sequence = "1010101010101010"
 
-# ランダムに入れ替えた01の数列を生成
-randomized_sequence = generate_random_binary_sequence(binary_sequence)
+    # ランダムに入れ替えた01の数列を生成
+    randomized_sequence = generate_random_binary_sequence(binary_sequence)
 
-print("Original sequence:", binary_sequence)
-print("Randomized sequence:", randomized_sequence)
+    print("Original sequence:", binary_sequence)
+    print("Randomized sequence:", randomized_sequence)
