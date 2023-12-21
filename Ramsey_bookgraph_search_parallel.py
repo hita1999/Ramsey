dfc2cb04257a54680806fd57c128eaf22f403e54
hita1999 <41143969+hita1999@users.noreply.github.com>
2023-12-21 14:32:22 +0900

    update: Refactor code for parallel processing and distance calculation

--- Ramsey_bookgraph_search_parallel.py
+++ Ramsey_bookgraph_search_parallel.py
@@ -1,8 +1,13 @@
 import itertools
 import random
 import numpy as np
-from multiprocessing import Pool, cpu_count
+import concurrent.futures
 
+def euclidean_distance(matrix1, matrix2):
+    flattened_matrix1 = matrix1.flatten()
+    flattened_matrix2 = matrix2.flatten()
+    distance = np.linalg.norm(flattened_matrix1 - flattened_matrix2)
+    return distance
 
 def read_adjacency_matrix(file_path):
     adjacency_matrix = []
@@ -12,43 +17,35 @@ def read_adjacency_matrix(file_path):
             adjacency_matrix.append(row)
     return np.array(adjacency_matrix)
 
-
-def check_combination(args):
-    matrix, indices, target_matrix, inverted_matrix = args
+def check_combination(matrix, indices, target_matrix):
     submatrix = matrix[np.ix_(indices, indices)]
-    return (np.array_equal(submatrix, target_matrix), np.array_equal(submatrix, inverted_matrix))
-
+    return np.array_equal(submatrix, target_matrix) or np.array_equal(submatrix, 1 - target_matrix)
 
 def generate_combinations(total, size):
     return itertools.combinations(range(total), size)
 
-
 def save_matrix_to_txt(matrix, file_path):
     np.savetxt(file_path, matrix, fmt='%d', delimiter='')
 
-
-def check_combinations_parallel(original_matrix, target_matrix, inverted_matrix, target_rows):
+def check_combinations(original_matrix, target_matrix, inverted_matrix, target_rows, bound_distance):
+    count = 0
+    inverted_count = 0
     indices_combinations = generate_combinations(len(original_matrix), target_rows)
 
-    # Use multiprocessing Pool for parallel processing
-    with Pool(processes=cpu_count()) as pool:
-        args = [(original_matrix, indices, target_matrix, inverted_matrix) for indices in indices_combinations]
-        results = pool.map(check_combination, args)
+    for indices in indices_combinations:
+        submatrix = original_matrix[np.ix_(indices, indices)]
+        if np.array_equal(submatrix, target_matrix):
+            count += 1
+        elif np.array_equal(submatrix, inverted_matrix):
+            inverted_count += 1
 
-    count = sum(result[0] for result in results)
-    inverted_count = sum(result[1] for result in results)
-
-    print(f"一致する組み合わせのカウント数", count)
-    print(f"反転した組み合わせのカウント数", inverted_count)
+    print(f"distance {bound_distance}以下のカウント数", count)
+    print(f"inverted distance {bound_distance}以下のカウント数", inverted_count)
 
     return count, inverted_count
 
-
 def generate_random_binary_sequence(binary_sequence_length):
-    random_sequence = np.random.choice([0, 1], size=binary_sequence_length)
-    print(random_sequence)
-    return random_sequence
-
+    return np.random.choice([0, 1], size=binary_sequence_length)
 
 def swap_rows_and_columns(original_matrix, random_sequence):
     idx = len(random_sequence)
@@ -59,6 +56,13 @@ def swap_rows_and_columns(original_matrix, random_sequence):
 
     return original_matrix
 
+def process_single_matrix(original_matrix, target_matrix, inverted_matrix, target_rows, bound_distance):
+    idx = random.randint(0, len(original_matrix) - 1)
+    new_sequence = generate_random_binary_sequence(len(original_matrix[idx][idx+1:]))
+    new_matrix = swap_rows_and_columns(original_matrix.copy(), new_sequence)
+    # print(new_matrix)
+    fisrt_count, first_inverted_count = check_combinations(new_matrix, target_matrix, inverted_matrix, target_rows, bound_distance)
+    return fisrt_count, first_inverted_count, new_matrix
 
 def main():
     file_path = 'adjcencyMatrix/K9_9-I.txt'
@@ -76,28 +80,27 @@ def main():
     np.fill_diagonal(second_inverted_matrix, 0)
     second_target_rows = second_target_matrix.shape[0]
 
+    bound_distance = 0
     count_sum = 1
 
-    while count_sum > 0:
-        idx = random.randint(0, len(original_matrix)-1)
-        new_sequence = generate_random_binary_sequence(len(original_matrix[idx][idx+1:]))
-        new_matrix = swap_rows_and_columns(original_matrix, new_sequence)
-        fisrt_count, first_inverted_count = check_combinations_parallel(new_matrix, first_target_matrix, first_inverted_matrix, first_target_rows)
-        first_count_sum = min(fisrt_count, first_inverted_count)
+    with concurrent.futures.ThreadPoolExecutor(max_workers=12) as executor:
+        futures = [executor.submit(process_single_matrix, original_matrix, first_target_matrix, first_inverted_matrix, first_target_rows, bound_distance) for _ in range(12)]
 
-        if first_count_sum == 0:
-            second_count, second_inverted_count = check_combinations_parallel(new_matrix, second_target_matrix, second_inverted_matrix, second_target_rows)
+        for future in concurrent.futures.as_completed(futures):
+            fisrt_count, first_inverted_count, new_matrix = future.result()
+            first_count_sum = min(fisrt_count, first_inverted_count)
 
-            if second_count == 0 and second_inverted_count == 0:
-                print("条件を満たすグラフが見つかりました")
-                break
-            else:
-                print("条件を満たすグラフは見つかりませんでした")
-                original_matrix = new_matrix
-                continue
+            if first_count_sum == 0:
+                second_count, second_inverted_count = check_combinations(new_matrix, second_target_matrix, second_inverted_matrix, second_target_rows, bound_distance)
 
-    save_matrix_to_txt(new_matrix, 'R(B3_B6_18).txt')
-
+                if second_count == 0 and second_inverted_count == 0:
+                    print("条件を満たすグラフが見つかりました")
+                    save_matrix_to_txt(new_matrix, 'R(B3_B6_18).txt')
+                    break
+                else:
+                    print("条件を満たすグラフは見つかりませんでした")
+                    original_matrix = new_matrix
+                    continue
 
 if __name__ == "__main__":
     main()
