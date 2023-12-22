a08860bec3e622b06e2898f6be4eef4b8680aa62
hita1999 <41143969+hita1999@users.noreply.github.com>
2023-12-23 00:02:46 +0900

    Refactor matrix checking and parallelize graph search

--- check_matrix_multiprocess.py
+++ check_matrix_multiprocess.py
@@ -1,8 +1,9 @@
 import itertools
-import numpy as np
-from multiprocessing import Pool, cpu_count
 from math import comb
+import numpy as np
+from multiprocessing import Pool
 from tqdm import tqdm
+import time
 
 def read_adjacency_matrix(file_path):
     adjacency_matrix = []
@@ -12,56 +13,69 @@ def read_adjacency_matrix(file_path):
             adjacency_matrix.append(row)
     return np.array(adjacency_matrix)
 
+def generate_matrix_candidates(total, size):
+    return itertools.combinations(range(total), size)
+
 def check_combination(matrix, indices, target_matrix):
     submatrix = matrix[np.ix_(indices, indices)]
     return np.array_equal(submatrix, target_matrix)
 
-def generate_matrix_candidates(total, size):
-    return itertools.combinations(range(total), size)
-
-def generate_matrix_candidates_with_progress(total, size):
-    total_combinations = comb(total, size)
-    chunk_size = total_combinations // cpu_count()
-    start = 0
-    end = chunk_size
-    for idx, indices in enumerate(tqdm(itertools.islice(generate_matrix_candidates(total, size), start, end), total=chunk_size, desc="Checking Matrix Candidates")):
-        yield indices
-        start = end
-        end += chunk_size
-
 def find_satisfying_graph_parallel(args):
-    matrix, target_matrix, target_rows = args
+    matrix, target_matrix, target_rows, start, end = args
 
-    for indices in generate_matrix_candidates_with_progress(len(matrix), target_rows):
+    found = False
+    for idx, indices in enumerate(itertools.islice(itertools.combinations(range(len(matrix)), target_rows), start, end)):
         if check_combination(matrix, indices, target_matrix):
-            return "target_matrix"
+            found = True
+            break
 
-    return False
+        # Update progress for each iteration
+        progress_bar.update(1)
+
+    return found
 
 def main():
-    file_path = 'adjcencyMatrix/T8.txt'
+    file_path = 'adjcencyMatrix/T9.txt'
     original_matrix = read_adjacency_matrix(file_path)
 
-    first_target_path = 'targetAdjcencyMatrix/B7.txt'
+    first_target_path = 'targetAdjcencyMatrix/B11.txt'
     first_target_matrix = read_adjacency_matrix(first_target_path)
     first_target_rows = first_target_matrix.shape[0]
 
-    second_target_path = 'targetAdjcencyMatrix/B6.txt'
-    second_target_matrix = read_adjacency_matrix(second_target_path)
-    second_target_rows = second_target_matrix.shape[0]
+    total_combinations = comb(len(original_matrix), first_target_rows)
+    
+    # 24 分割
+    num_processes = 24
+    chunk_size = total_combinations // num_processes
+    
+    # 各プロセスに適切な範囲を割り当てる
+    args_list = [
+        (original_matrix, first_target_matrix, first_target_rows, start, start + chunk_size)
+        for start in range(0, total_combinations, chunk_size)
+    ]
 
-    args_list = [(original_matrix, first_target_matrix, first_target_rows),
-                 (original_matrix, second_target_matrix, second_target_rows)]
+    # 進捗バーの作成
+    global progress_bar
+    progress_bar = tqdm(total=total_combinations, desc="Checking Matrix Candidates")
 
-    with Pool(processes=cpu_count()) as pool:
-        results = list(pool.imap_unordered(find_satisfying_graph_parallel, args_list))
+    start_time = time.time()
 
-    for idx, satisfying_graph_type in enumerate(results):
-        if satisfying_graph_type == "target_matrix":
-            print(f"{file_path}には{first_target_path if idx == 0 else second_target_path}が見つかりました")
-            print(f"{idx + 1}_graphにて条件を満たすグラフが見つかりました ({'first' if idx == 0 else 'second'}_target_matrix)")
-        else:
-            print(f"{file_path}には{first_target_path if idx == 0 else second_target_path}は見つかりませんでした")
+    with Pool(processes=num_processes) as pool:
+        results = list(pool.imap(find_satisfying_graph_parallel, args_list))
+
+    elapsed_time = time.time() - start_time
+
+    progress_bar.close()
+
+    # Check results
+    if any(results):
+        print(f"{file_path}には{first_target_path}が見つかりました")
+        print("条件を満たすグラフが見つかりました (first_target_matrix)")
+    else:
+        print(f"{file_path}には{first_target_path}は見つかりませんでした")
+
+    print(f"\nTotal elapsed time: {elapsed_time:.2f} seconds")
+    print(f"Iteration speed: {total_combinations / elapsed_time:.2f} iterations per second")
 
 if __name__ == "__main__":
     main()
