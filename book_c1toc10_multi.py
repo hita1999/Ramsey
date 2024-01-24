e69a6ac16dd7d36b0f5a7c9261e9a1e5bf7b4c82
hita1999 <41143969+hita1999@users.noreply.github.com>
2024-01-25 02:23:51 +0900

    Major Add: c1toc10

---
+++ book_c1toc10_multi.py
@@ -0,0 +1,202 @@
+from calendar import c
+import datetime
+from multiprocessing import Pool, cpu_count, Manager
+from tqdm import tqdm
+import numpy as np
+from numba import jit
+
+@jit(nopython=True, cache=True)
+def set_indices(target_matrix, target_size, condition):
+    n = len(target_matrix)
+    ret = -1
+
+    for i in range(n - 1):
+        for j in range(i + 1, n):
+            spine_indices = np.array([i, j], dtype=np.int64)
+
+            row_1 = target_matrix[spine_indices[0], :]
+            row_2 = target_matrix[spine_indices[1], :]
+
+            if condition == 2 and (target_matrix[i, j] == 1):
+                common_vertices = np.where((row_1 == 1) & (row_2 == 1))[0]
+                if len(common_vertices) < target_size:
+                    ret = 0
+                else:
+                    return -1
+
+            if condition == 0 and (target_matrix[i, j] == 0):
+                common_vertices = np.where((row_1 == 0) & (row_2 == 0))[0]
+                common_vertices = np.array([x for x in common_vertices if x not in spine_indices])
+                if len(common_vertices) < target_size:
+                    ret = 0
+                else:
+                    return -1
+
+    return ret
+
+@jit(nopython=True, cache=True)
+def integer_to_binary(cir_size, i):
+    binary_array = np.zeros(cir_size, dtype=np.uint8)
+    for j in range(cir_size-1, -1, -1):
+        binary_array[cir_size - 1 - j] = np.right_shift(i, j) & 1
+    return binary_array
+
+@jit(nopython=True, cache=True)
+def triu_numba(matrix):
+    m, n = matrix.shape
+    result = np.zeros_like(matrix)
+
+    for i in range(m):
+        for j in range(i, n):
+            result[i, j] = matrix[i, j]
+
+    return result
+
+@jit(nopython=True, cache=True)
+def circulant_numba(c):
+    c = np.asarray(c).ravel()
+    L = len(c)
+    result = np.zeros((L, L), dtype=c.dtype)
+
+    for i in range(L):
+        for j in range(L):
+            result[i, j] = c[(i - j) % L]
+
+    return result
+
+@jit(nopython=True, cache=True)
+def assign_matrix_to_A(A, matrix, row_start, row_end, col_start, col_end):
+    A[row_start:row_end, col_start:col_end] = matrix
+
+def save_matrix_to_txt(matrix, file_path):
+    np.savetxt(file_path, matrix, fmt='%d', delimiter='')
+
+def calculate_A(args):
+    matrix_size, matrix_index, first_target_size, second_target_size, found = args
+    
+    A = np.zeros((matrix_size, matrix_size), dtype=np.uint8)
+    
+    counter = 0
+    vector = integer_to_binary(matrix_size // 4, matrix_index)
+    C1 = circulant_numba(vector)
+    C1 = triu_numba(C1) + triu_numba(C1).T
+
+    assign_matrix_to_A(A, C1, 0, matrix_size//4, 0, matrix_size//4)
+    
+    for matrix2_index in range(2 ** (matrix_size // 4 - 1)):
+        vector2 = integer_to_binary(matrix_size // 4, matrix2_index)
+        C2 = circulant_numba(vector2)
+        C2 = triu_numba(C2) + triu_numba(C2).T
+
+        assign_matrix_to_A(A, C2, 0, matrix_size//4, matrix_size//4, 2*matrix_size//4)
+        assign_matrix_to_A(A, C2.T, matrix_size//4, 2*matrix_size//4, 0, matrix_size//4)
+
+        for matrix3_index in range(2 ** (matrix_size // 4 - 1)):
+            vector3 = integer_to_binary(matrix_size // 4, matrix3_index)
+            C3 = circulant_numba(vector3)
+            C3 = triu_numba(C3) + triu_numba(C3).T
+
+            assign_matrix_to_A(A, C3, 0, matrix_size//4, 2*matrix_size//4, 3*matrix_size//4)
+            assign_matrix_to_A(A, C3.T, 2*matrix_size//4, 3*matrix_size//4, 0, matrix_size//4)
+            
+            for matrix4_index in range(2 ** (matrix_size // 4 - 1)):
+                vector4 = integer_to_binary(matrix_size // 4, matrix4_index)
+                C4 = circulant_numba(vector4)
+                C4 = triu_numba(C4) + triu_numba(C4).T
+
+                assign_matrix_to_A(A, C4, 0, matrix_size//4, 3*matrix_size//4, matrix_size)
+                assign_matrix_to_A(A, C4.T, 3*matrix_size//4, matrix_size, 0, matrix_size//4)
+
+                for matrix5_index in range(2 ** (matrix_size // 4 - 1)):
+                    vector5 = integer_to_binary(matrix_size // 4, matrix5_index)
+                    C5 = circulant_numba(vector5)
+                    C5 = triu_numba(C5) + triu_numba(C5).T
+
+                    assign_matrix_to_A(A, C5, matrix_size//4, 2*matrix_size//4, matrix_size//4, 2*matrix_size//4)
+                    
+                    for matrix6_index in range(2 ** (matrix_size // 4 - 1)):
+                        vector6 = integer_to_binary(matrix_size // 4, matrix6_index)
+                        C6 = circulant_numba(vector6)
+                        C6 = triu_numba(C6) + triu_numba(C6).T
+
+                        assign_matrix_to_A(A, C6, matrix_size//4, 2*matrix_size//4, 2*matrix_size//4, 3*matrix_size//4)
+                        assign_matrix_to_A(A, C6.T, 2*matrix_size//4, 3*matrix_size//4, matrix_size//4, 2*matrix_size//4)
+                        
+                        for matrix7_index in range(2 ** (matrix_size // 4 - 1)):
+                            vector7 = integer_to_binary(matrix_size // 4, matrix7_index)
+                            C7 = circulant_numba(vector7)
+                            C7 = triu_numba(C7) + triu_numba(C7).T
+
+                            assign_matrix_to_A(A, C7, matrix_size//4, 2*matrix_size//4, 3*matrix_size//4, matrix_size)
+                            assign_matrix_to_A(A, C7.T, 3*matrix_size//4, matrix_size, matrix_size//4, 2*matrix_size//4)
+
+                            for matrix8_index in range(2 ** (matrix_size // 4 - 1)):
+                                vector8 = integer_to_binary(matrix_size // 4, matrix8_index)
+                                C8 = circulant_numba(vector8)
+                                C8 = triu_numba(C8) + triu_numba(C8).T
+
+                                assign_matrix_to_A(A, C8, 2*matrix_size//4, 3*matrix_size//4, 2*matrix_size//4, 3*matrix_size//4)
+
+                                for matrix9_index in range(2 ** (matrix_size // 4 - 1)):
+                                    vector9 = integer_to_binary(matrix_size // 4, matrix9_index)
+                                    C9 = circulant_numba(vector9)
+                                    C9 = triu_numba(C9) + triu_numba(C9).T
+
+                                    assign_matrix_to_A(A, C9, 2*matrix_size//4, 3*matrix_size//4, 3*matrix_size//4, matrix_size)
+                                    assign_matrix_to_A(A, C9.T, 3*matrix_size//4, matrix_size, 2*matrix_size//4, 3*matrix_size//4)
+
+                                    for matrix10_index in range(2 ** (matrix_size // 4 - 1)):
+                                        vector10 = integer_to_binary(matrix_size // 4, matrix10_index)
+                                        C10 = circulant_numba(vector10)
+                                        C10 = triu_numba(C10) + triu_numba(C10).T
+
+                                        assign_matrix_to_A(A, C10, 3*matrix_size//4, matrix_size, 3*matrix_size//4, matrix_size)
+                                        counter += 1
+
+                                        ret = set_indices(A, first_target_size, 2)
+                                        if ret == 0:
+                                            ret2 = set_indices(A, second_target_size, 0)
+                                            if ret2 == 0:
+                                                decimal_value = sum(int(''.join(map(str, vec)), 2) * (2 ** (matrix_size // 5 - 1)) ** exp for vec, exp in zip([vector, vector2, vector3, vector4, vector5, vector6, vector7, vector8, vector9, vector10], [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]))
+                                                return A, vector, vector2, vector3, vector4, vector5, vector6, vector7, vector8, vector9, vector10, decimal_value
+
+    return None
+
+def main():
+    manager = Manager()
+    found = manager.Value('b', False)  # 'b' stands for boolean
+
+    first_target_size = int(input("first_target_book: "))
+    second_target_size = int(input("second_target_book: "))
+    matrix_size = int(input("matrix_size: "))
+
+    print('Max', (2 ** (matrix_size // 4 - 1))**10)
+
+    args_list = [(matrix_size, matrix_index, first_target_size, second_target_size, found) for matrix_index in range(2 ** (matrix_size // 4 - 1))]
+
+    with Pool() as pool:
+        for result in tqdm(pool.imap_unordered(calculate_A, args_list), total=len(args_list), desc="Finding satisfying graph", position=0, leave=True):
+            if result is not None:
+                A, vector, vector2, vector3, vector4, vector5, vector6, vector7, vector8, vector9, vector10, decimal_value = result
+                print('found!')
+                save_matrix_to_txt(A, f'generatedMatrix/circulantBlock/C1toC10_{decimal_value}.txt')
+                print(decimal_value)
+                print(vector)
+                print(vector2)
+                print(vector3)
+                print(vector4)
+                print(vector5)
+                print(vector6)
+                print(vector7)
+                print(vector8)
+                print(vector9)
+                print(vector10)
+                found.value = True
+                break
+
+    if not found.value:
+        print('not found')
+
+if __name__ == "__main__":
+    print(datetime.datetime.now().time())
+    main()
