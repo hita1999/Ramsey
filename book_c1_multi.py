b4737e2da6fe1a8b1238381998befbf49db5ccbc
hita1999 <41143969+hita1999@users.noreply.github.com>
2024-01-26 01:21:45 +0900

    Refactor code for readability and performance

--- book_c1_multi.py
+++ book_c1_multi.py
@@ -4,7 +4,7 @@ from tqdm import tqdm
 import numpy as np
 from scipy.linalg import circulant
 from numba import jit
-import sys
+
 
 @jit(nopython=True)
 def set_indices(target_matrix, target_size, condition):
@@ -27,13 +27,15 @@ def set_indices(target_matrix, target_size, condition):
 
             if condition == 0 and (target_matrix[i, j] == 0):
                 common_vertices = np.where((row_1 == 0) & (row_2 == 0))[0]
-                common_vertices = np.array([x for x in common_vertices if x not in spine_indices])
+                common_vertices = np.array(
+                    [x for x in common_vertices if x not in spine_indices])
                 if len(common_vertices) < target_size:
                     ret = 0
                 else:
                     return -1
 
     return ret
+
 
 @jit(nopython=True)
 def integer_to_binary(cir_size, binary_index):
@@ -42,8 +44,10 @@ def integer_to_binary(cir_size, binary_index):
         binary_array[cir_size - 1 - j] = np.right_shift(binary_index, j) & 1
     return binary_array
 
+
 def save_matrix_to_txt(matrix, file_path):
     np.savetxt(file_path, matrix, fmt='%d', delimiter='')
+
 
 def calculate_A_batch(args):
     start, end, matrix_size, first_target_size, second_target_size, found, result_queue = args
@@ -54,16 +58,18 @@ def calculate_A_batch(args):
         if i >= 2**(matrix_size-1):
             break
         vector = integer_to_binary(matrix_size, i)
-        counter += calculate_A((vector, first_target_size, second_target_size, found, result_list))
+        counter += calculate_A((vector, first_target_size,
+                               second_target_size, found, result_list))
 
     result_queue.put(result_list)
     return counter
+
 
 def calculate_A(args):
     vector, first_target_size, second_target_size, found, result_list = args
     C1 = circulant(vector)
     C1 = np.triu(C1) + np.triu(C1, 1).T
-    
+
     ret = set_indices(C1, first_target_size, 2)
     if ret == 0:
         ret2 = set_indices(C1, second_target_size, 0)
@@ -71,9 +77,11 @@ def calculate_A(args):
             decimal_value = int(''.join(map(str, vector)), 2)
             result_list.append((C1, decimal_value))
             found.value = True
-            save_matrix_to_txt(C1, f'generatedMatrix/circulantBlock/C1_{decimal_value}.txt')
+            save_matrix_to_txt(
+                C1, f'generatedMatrix/circulantBlock/C1_{decimal_value}.txt')
             print(decimal_value)
     return 1
+
 
 def main():
     with Manager() as manager:
@@ -83,12 +91,13 @@ def main():
         first_target_size = int(input("first_target_book: "))
         second_target_size = int(input("second_target_book: "))
         matrix_size = int(input("matrix_size: "))
-        
+
         print('Max', (2 ** (matrix_size - 1)))
         chunk_size = cpu_count() * 16
 
-        ranges = ((i, i + chunk_size, matrix_size, first_target_size, second_target_size, found, result_queue) for i in range(0, 2**(matrix_size-1), chunk_size))
-        
+        ranges = ((i, i + chunk_size, matrix_size, first_target_size, second_target_size,
+                  found, result_queue) for i in range(0, 2**(matrix_size-1), chunk_size))
+
         with Pool() as pool:
             for _ in tqdm(pool.imap_unordered(calculate_A_batch, ranges), total=2**(matrix_size-1)//chunk_size, desc="Finding satisfying graph"):
                 if found.value:
@@ -100,6 +109,7 @@ def main():
 
         if not found.value:
             print('not found')
+
 
 if __name__ == "__main__":
     main()
