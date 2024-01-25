ee2b0dccabaec806348a4a248d8ae2494dcd6d80
hita1999 <41143969+hita1999@users.noreply.github.com>
2024-01-25 19:58:50 +0900

    Refactor calculate_A_batch function to use a result_list and result_queue

--- book_c1_multi.py
+++ book_c1_multi.py
@@ -44,21 +44,22 @@ def integer_to_binary(cir_size, binary_index):
 def save_matrix_to_txt(matrix, file_path):
     np.savetxt(file_path, matrix, fmt='%d', delimiter='')
 
-
 def calculate_A_batch(args):
-    start, end, matrix_size, first_target_size, second_target_size, found = args
+    start, end, matrix_size, first_target_size, second_target_size, found, result_queue = args
     counter = 0
+    result_list = []
 
     for i in range(start, end):
         if i >= 2**(matrix_size-1):
             break
         vector = integer_to_binary(matrix_size, i)
-        counter += calculate_A((vector, first_target_size, second_target_size, found))
+        counter += calculate_A((vector, first_target_size, second_target_size, found, result_list))
 
+    result_queue.put(result_list)
     return counter
 
 def calculate_A(args):
-    vector, first_target_size, second_target_size, found = args
+    vector, first_target_size, second_target_size, found, result_list = args
     C1 = circulant(vector)
     C1 = np.triu(C1) + np.triu(C1, 1).T
     
@@ -66,17 +67,15 @@ def calculate_A(args):
     if ret == 0:
         ret2 = set_indices(C1, second_target_size, 0)
         if ret2 == 0:
-            print('found!')
             decimal_value = int(''.join(map(str, vector)), 2)
-            save_matrix_to_txt(C1, f'generatedMatrix/circulantBlock/C1_{decimal_value}.txt')
-            print(decimal_value)
-            print(vector)
+            result_list.append((C1, decimal_value))
             found.value = True
     return 1
 
 def main():
     with Manager() as manager:
         found = manager.Value('b', False)
+        result_queue = manager.Queue()
 
         first_target_size = int(input("first_target_book: "))
         second_target_size = int(input("second_target_book: "))
@@ -85,13 +84,22 @@ def main():
         print('Max', (2 ** (matrix_size - 1)))
         chunk_size = cpu_count() * 16
 
-        ranges = [(i, i + chunk_size, matrix_size, first_target_size, second_target_size, found) for i in range(0, 2**(matrix_size-1), chunk_size)]
+        ranges = ((i, i + chunk_size, matrix_size, first_target_size, second_target_size, found, result_queue) for i in range(0, 2**(matrix_size-1), chunk_size))
         
         with Pool() as pool:
-            for _ in tqdm(pool.imap_unordered(calculate_A_batch, ranges), total=len(ranges), desc="Finding satisfying graph"):
+            for _ in tqdm(pool.imap_unordered(calculate_A_batch, ranges), total=2**(matrix_size-1)//chunk_size, desc="Finding satisfying graph"):
                 pass
 
-        if not found.value:
+        result_list = []
+        while not result_queue.empty():
+            result_list.extend(result_queue.get())
+
+        if found.value:
+            for matrix, decimal_value in result_list:
+                save_matrix_to_txt(matrix, f'generatedMatrix/circulantBlock/C1_{decimal_value}.txt')
+                print(decimal_value)
+
+        else:
             print('not found')
 
 if __name__ == "__main__":
