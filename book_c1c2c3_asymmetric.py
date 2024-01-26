ec8bc3e51c21e8c4988e7c3172e2115bdf64b28e
hita1999 <41143969+hita1999@users.noreply.github.com>
2024-01-26 20:48:32 +0900

    Refactor matrix size calculations and variable names

--- book_c1c2c3_asymmetric.py
+++ book_c1c2c3_asymmetric.py
@@ -48,17 +48,18 @@ def integer_to_binary(cir_size, i):
 def diagonal_integer_to_binary(matrix_size, i):
     cir_size = matrix_size // 4
     r = matrix_size % 4
-    
+
     binary_array = np.zeros(cir_size, dtype=np.uint8)
     for j in range(cir_size-1, -1, -1):
         binary_array[cir_size - 1 - j] = np.right_shift(i, j) & 1
 
     reversed_binary_array = np.flip(binary_array)
-    
+
     if r == 0:
         combined_array = np.concatenate([binary_array, reversed_binary_array])
     else:
-        combined_array = np.concatenate([[0], binary_array, reversed_binary_array])
+        combined_array = np.concatenate(
+            [[0], binary_array, reversed_binary_array])
 
     return combined_array
 
@@ -99,7 +100,7 @@ def calculate_A(args):
 
     # C2
     for matrix2_index in range(2 ** (matrix_size // 2)):
-        vector2 = integer_to_binary(matrix_size // 2, matrix2_index)
+        vector2 = integer_to_binary(matrix_size, matrix2_index)
 
         C2 = circulant_numba(vector2)
 
@@ -110,7 +111,7 @@ def calculate_A(args):
 
         for matrix3_index in range(2 ** (matrix_size // 4)):
             vector3 = diagonal_integer_to_binary(
-                matrix_size // 4, matrix3_index)
+                matrix_size, matrix3_index)
             C3 = circulant_numba(vector3)
 
             assign_matrix_to_A(A, C3, matrix_size//2,
