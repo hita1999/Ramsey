857d9870d10c39515991ed63a4f14ce9454b5bbc
hita1999 <41143969+hita1999@users.noreply.github.com>
2024-02-29 15:02:35 +0900

    コードの変更: ファイルパスを更新し、新しい行を追加

--- formerCheckMatrix/count_edges.py
+++ formerCheckMatrix/count_edges.py
@@ -9,7 +9,7 @@ def read_matrix_from_file(file_path):
     return np.array(matrix)
 
 if __name__ == '__main__':
-    file_path = 'adjcencyMatrix/Paley/Paley17.txt'
+    file_path = 'generatedMatrix/circulantBlock/cir42B10B11/C1C2C3_asymmetric_10287365374.txt'
     matrix = read_matrix_from_file(file_path)
 
     # 上三角部分を抽出
@@ -18,4 +18,9 @@ if __name__ == '__main__':
     # 辺の総数を計算
     total_edges = np.count_nonzero(upper_triangle == 1)
 
-    print("辺の総数:", total_edges)
+    print("総数:", int(matrix.shape[0] * (matrix.shape[0] - 1) / 2))
+    print("redの総数:", total_edges)
+    print("blueの総数:", int(matrix.shape[0] * (matrix.shape[0] - 1) / 2) - total_edges)
+    
+    print('割合:', total_edges / (matrix.shape[0] * (matrix.shape[0] - 1) / 2)*100)
+    print('割合:', int(total_edges / (matrix.shape[0] * (matrix.shape[0] - 1) / 2)*100))
